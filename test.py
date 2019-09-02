4d9601c3c15058ba10c2c6fc640653bdcad6c037
Gloria <stanciugloria@gmail.com>
2019-09-02 18:34:56 +0300

    test

---
+++ test.py
@@ -0,0 +1,34 @@
+import os
+import datetime
+
+
+print("Processors: ")
+with open("/proc/cpuinfo", "r")  as f:
+    info = f.readlines()
+
+cpuinfo = [x.strip().split(":")[1] for x in info if "model name"  in x]
+for index, item in enumerate(cpuinfo):
+    print("    " + str(index) + ": " + item)
+
+def getRAMinfo():
+    p = os.popen('free')
+    i = 0
+    while 1:
+        i = i + 1
+        line = p.readline()
+        if i==2:
+            return(line.split()[1:4])
+
+print("\nRAM Memory: ")
+# Output is in kb, here I convert it in Mb for readability
+RAM_stats = getRAMinfo()
+RAM_total = round(int(RAM_stats[0]) / 1000,1)
+RAM_used = round(int(RAM_stats[1]) / 1000,1)
+RAM_free = round(int(RAM_stats[2]) / 1000,1)
+
+print("Total RAM:", RAM_total)
+print("Used RAM:", RAM_used)
+print("Free RAM:", RAM_free)
+
+print("\nTimestamp ISO format: ")
+print(datetime.datetime.now().replace(microsecond=0).isoformat())
