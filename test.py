ed57e940495a78f7a7fccff4dcad7b10f5e70739
Gloria <stanciugloria@gmail.com>
2019-09-04 16:01:41 +0300

    test pi

--- test.py
+++ test.py
@@ -1,16 +1,18 @@
+import psutil
+import time
 import os
 import datetime
+import json
 
+def frequency():
+    print("Processors: ")
+    cpu_usage = psutil.cpu_percent()
+    print(cpu_usage)
+    time.sleep(1)
 
-print("Processors: ")
-with open("/proc/cpuinfo", "r")  as f:
-    info = f.readlines()
-
-cpuinfo = [x.strip().split(":")[1] for x in info if "model name"  in x]
-for index, item in enumerate(cpuinfo):
-    print("    " + str(index) + ": " + item)
 
 def getRAMinfo():
+
     p = os.popen('free')
     i = 0
     while 1:
@@ -19,16 +21,54 @@ def getRAMinfo():
         if i==2:
             return(line.split()[1:4])
 
-print("\nRAM Memory: ")
-# Output is in kb, here I convert it in Mb for readability
+def ram():
+
+    print("\nRAM Memory: ")
+    # Output is in kb, here I convert it in Mb for readability
+    RAM_stats = getRAMinfo()
+    RAM_total = round(int(RAM_stats[0]) / 1000,1)
+    RAM_used = round(int(RAM_stats[1]) / 1000,1)
+    RAM_free = round(int(RAM_stats[2]) / 1000,1)
+    print("\tTotal RAM: ", RAM_total)
+    print("\tUsed RAM: ", RAM_used)
+    print("\tFree RAM: ", RAM_free)
+
+def timestamp():
+
+    print("\nTimestamp ISO format: ")
+    print("\t", datetime.datetime.now().replace(microsecond=0).isoformat())
+
+def diskUsage():
+    
+    print("\nDisk usage")
+    disk = psutil.disk_usage('/')
+    diskTotal = disk.total / 2**30     # GiB.
+    diskUsed = disk.used / 2**30
+    diskFree = disk.free / 2**30
+    print("\tDisk total [GiB]:", diskTotal)
+    print("\tDisk used [GiB]:", diskUsed)
+    print("\tDisk free [GiB]:", diskFree)
+
+# while True :
+#     frequency()
+#     ram()
+#     timestamp()
+#     diskUsage()
+
 RAM_stats = getRAMinfo()
-RAM_total = round(int(RAM_stats[0]) / 1000,1)
-RAM_used = round(int(RAM_stats[1]) / 1000,1)
-RAM_free = round(int(RAM_stats[2]) / 1000,1)
+disk = psutil.disk_usage('/')
 
-print("Total RAM:", RAM_total)
-print("Used RAM:", RAM_used)
-print("Free RAM:", RAM_free)
+hwInfo = {
+    "cpuUsage": psutil.cpu_percent(),
+    "totalRAM": round(int(RAM_stats[0]) / 1000,1),
+    "usedRAM": round(int(RAM_stats[1]) / 1000,1),
+    "freeRAM": round(int(RAM_stats[2]) / 1000,1),
+    "timeStamp": datetime.datetime.now().replace(microsecond=0).isoformat(),
+    "diskTotal": disk.total / 2**30,
+    "diskUsed": disk.used / 2**30,
+    "diskFree": disk.free / 2**30
+}
 
-print("\nTimestamp ISO format: ")
-print(datetime.datetime.now().replace(microsecond=0).isoformat())
+info = json.dumps(hwInfo, indent=4)
+
+print(info)
